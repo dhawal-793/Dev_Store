58d9f8523f0a3687e514bd95ef98d693181c8e88
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-26 12:38:44 +0530

    Added Navbar inside layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 
+import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 
 import './globals.css'
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
         <Footer />
       </body>
