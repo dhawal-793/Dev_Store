10ba35be8a348e0e3aa52f0083ac06916cadb9e6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-26 12:27:32 +0530

    Created method to fetch all the categories.

---
+++ actions/get-cetegories.tsx
@@ -0,0 +1,11 @@
+import { Category } from "@/types"
+
+const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`
+
+const fetchCategories = async (): Promise<Category[]> => {
+    const res = await fetch(url)
+    return res.json()
+    
+}
+
+export default fetchCategories
