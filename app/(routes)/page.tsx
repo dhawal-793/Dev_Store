ebd5f3d1a369d64eb677f2c06c7c392124d21cf3
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-27 22:34:34 +0530

    Fetched and added Billboard on Home page.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,20 @@
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import fetchBillboard from "@/actions/get-billboard";
 
-export default function Home() {
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+
+const Home = async () => {
+  const billboard = await fetchBillboard('4bac3797-456b-41ef-84e6-0d8a64093544');
+
   return (
-    <main>
-      Home Page
-    </main>
+    <Container>
+      <div className="pb-10 space-y-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
+
+export default Home
