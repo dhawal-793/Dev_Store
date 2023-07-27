e7636140bfef0ab69d84828dc3be3e650f72411f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-27 22:41:24 +0530

    Fetched featuredProducts and added ProductList.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,17 +1,24 @@
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import fetchBillboard from "@/actions/get-billboard";
+import fetchProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 export const dynamic = 'force-dynamic';
 
 const Home = async () => {
+
+  const featuredProducts = await fetchProducts({ isFeatured: true })
   const billboard = await fetchBillboard('4bac3797-456b-41ef-84e6-0d8a64093544');
 
   return (
     <Container>
       <div className="pb-10 space-y-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={featuredProducts} />
       </div>
     </Container>
   )
