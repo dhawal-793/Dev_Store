157ab34fac790de06260490edc5af029cece0eaf
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-27 22:51:47 +0530

    Added NoResults component inside productList.

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 
 import { Product } from '@/types';
+import NoResults from '@/components/ui/no-results';
 
 interface ProductListProps {
     title: string;
@@ -12,6 +13,8 @@ const ProductList: FC<ProductListProps> = ({ title, items }) => {
     return (
         <div className='space-y-4'>
             <h3>{title}</h3>
+            {items.length === 0 && <NoResults />}
+
         </div>
     )
 }
