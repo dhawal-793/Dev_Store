793b26947aa6edc58ab6fa68ef9e80b788e6b653
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-30 09:36:20 +0530

    Updated constant name from url to URL.

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -1,9 +1,9 @@
 import { Product } from "@/types"
 
-const url = `${process.env.NEXT_PUBLIC_API_URL}/products`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
 
 const fetchProduct = async (id: String): Promise<Product> => {
-    const res = await fetch(`${url}/${id}`)
+    const res = await fetch(`${URL}/${id}`)
     return res.json()
 
 }
