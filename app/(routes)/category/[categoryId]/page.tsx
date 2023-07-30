0b67f14fe146ada3ba67e8167468f82e118d5965
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-30 09:58:14 +0530

    Fetched products, colors, category, sizes.

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,16 +1,39 @@
 import { FC } from 'react'
 
+import fetchCategory from '@/actions/get-category';
+import fetchColors from '@/actions/get-colors';
+import fetchProducts from '@/actions/get-products';
+import fetchSizes from '@/actions/get-sizes';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 interface CategoryPageProps {
     params: {
-        categoryId: string
+        categoryId: string;
+    },
+    searchParams: {
+        colorId?: string;
+        sizeId?: string;
     }
 }
 
-const CategoryPage: FC<CategoryPageProps> = ({ params: { categoryId } }) => {
+const CategoryPage: FC<CategoryPageProps> = async ({ params: { categoryId }, searchParams: { colorId, sizeId } }) => {
+
+    const products = await fetchProducts({ categoryId, colorId, sizeId })
+    const colors = await fetchColors();
+    const sizes = await fetchSizes();
+    const category = await fetchCategory(categoryId);
+
 
     return (
-        <div>
-            Category Page
+        <div className='bg-white'>
+            <Container>
+                <div>
+                    Category
+                </div>
+            </Container>
         </div>
     )
 }
