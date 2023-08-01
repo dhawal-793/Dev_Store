5d19c484d6aa4b0d7ddf9086144bf8685d77b47e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-01 09:45:59 +0530

    Added check for mounting

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,4 +1,15 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
 const CartPage = () => {
+    const [isMonted, setIsMonted] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsMonted(true)
+    }, [])
+
+    if (!isMonted) return null
 
     return (
         <div>page</div>
