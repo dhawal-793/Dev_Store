4301030eebf972649033a851f06930f7fa9c5022
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-16 22:03:58 +0530

    removed the billboard name from the image.

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -13,11 +13,11 @@ const Billboard: FC<BillboardProps> = ({ data }) => {
                 className='relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover'
                 style={{ backgroundImage: `url(${data?.imageUrl})` }}
             >
-                <div className="flex flex-col items-center justify-center w-full h-full text-center gap-y-8">
+                {/* <div className="flex flex-col items-center justify-center w-full h-full text-center gap-y-8">
                     <div className="max-w-xs text-3xl font-bold sm:text-5xl lg:text-6xl sm:max-w-xl ">
                         {data.label}
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
