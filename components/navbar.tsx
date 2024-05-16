e1bf1d428dac257682087f01e552e7d6300b3824
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-16 22:06:16 +0530

    added horizontally scrollable menu for small screens.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,19 +13,30 @@ const Navbar = async () => {
     const categories = await fetchCategories()
 
     return (
-        <div className='border-b'>
-            <Container>
-                <div className='relative flex items-center h-16 px-4 sm:px-6 lg:px-0'>
-                    <Link href="/" className='flex ml-4 lg:ml-0 gap-x-2'>
-                        <p className='text-xl font-bold'>
-                            DevStore
-                        </p>
-                    </Link>
-                    <MainNav data={categories} />
-                    <NavbarActions />
-                </div>
-            </Container>
-        </div>
+        <>
+            <div className='border-b'>
+                <Container>
+                    <div className='relative flex items-center h-16 px-4 sm:px-6 lg:px-0'>
+                        <Link href="/" className='flex ml-4 lg:ml-0 gap-x-2'>
+                            <p className='text-2xl font-bold'>
+                                DevStore
+                            </p>
+                        </Link>
+                        <div className='hidden lg:block'>
+                            <MainNav data={categories} />
+                        </div>
+                        <NavbarActions />
+                    </div>
+                </Container>
+            </div>
+            <div className='my-5 lg:hidden px-4'>
+                <Container className='rounded-lg '>
+                    <div className='w-full mx-auto'>
+                        <MainNav data={categories} className='w-full mx-auto overflow-scroll custom-scroll pb-2 px-2' />
+                    </div>
+                </Container>
+            </div>
+        </>
     )
 }
 
